6df22edaa70da9a6f7d4dd77f772b02a81dce395
Edcleison <edcleisonbrito@gmail.com>
2023-11-08 00:24:23 -0300

    first commit

---
+++ AlunosApi/Services/IAlunoService.cs
@@ -0,0 +1,17 @@
+﻿using AlunosApi.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AlunosApi.Services
+{
+    public interface IAlunoService
+    {
+        Task<IEnumerable<Aluno>> GetAlunos();
+        Task<Aluno> GetAluno(int id);
+        Task<IEnumerable<Aluno>> GetAlunoByNome(string nome);
+        Task CreateAluno(Aluno aluno);
+        Task UpdateAluno(Aluno aluno);
+        Task DeleteAluno(Aluno aluno);
+    }
+}
